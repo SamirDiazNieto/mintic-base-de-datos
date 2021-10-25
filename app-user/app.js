4b20b6afa2c02b5ea8ae2880e74c90a98b2480b3
jeisone <jeisoneduardoep@ufps.edu.co>
2021-10-24 20:44:18 -0500

    cambios samir firebase

--- app-user/app.js
+++ app-user/app.js
@@ -3,10 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
+var cors = require('cors')
 const swaggerJSDoc = require('swagger-jsdoc');  
 const swaggerUI = require('swagger-ui-express');
+var admin = require("firebase-admin");
+var serviceAccount = require("./config/firebase/wb-js-eea5b-firebase-adminsdk-irpdw-1b36499119.json");
 
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
  var MongoDBUtil = require('./modules/mongodb/modules/mongodb.module').MongoDBUtil;
 
@@ -34,7 +41,7 @@ var app = express();
  const swaggerDocs = swaggerJSDoc(swaggerOptions);  
  app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 
-
+ app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,7 +49,22 @@ app.use(cookieParser());
 
 MongoDBUtil.init();
 
- app.use(cors())
+
+function checkAuth(req, res, next) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+    admin.auth().verifyIdToken(idToken)
+      .then(() => {
+        next()
+      }).catch((error) => {
+        res.status(403).send('Unauthorized')
+      });
+  } else {
+    res.status(401).send('Unauthorized')
+  }
+}
+ //
+ //  app.use('*',checkAuth);
  app.use('/usuarios', UsuarioController);
  app.use('/customersVentas', CustomerControllerV);
  app.use('/customers', CustomerController);
